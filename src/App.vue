6d1c6a9a3ee91022252a4e1f8f723335327b41dc
hanosong <838519800@qq.com>
2023-07-26 19:59:04 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const arr:string[] = ['nba', 'cba', 'aaa']
+</script>
+
+<template>
+  <div v-for="(item, index) in arr">
+    {{ item }} - {{ index }}
+  </div>
+</template>
+
+<style scoped>
+</style>
