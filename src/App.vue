dfcb4489d8b5c97d342b9a741b3e9d8e56445509
hanosong <838519800@qq.com>
2023-08-08 10:08:58 +0800

    bem架构和安装sass

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-  const arr:string[] = ['nba', 'cba', 'aaa']
+const arr: string[] = ["nba", "cba", "aaa"];
 </script>
 
 <template>
-  <div v-for="(item, index) in arr">
-    {{ item }} - {{ index }}
-  </div>
+  <div v-for="(item, index) in arr">{{ item }} - {{ index }}</div>
 </template>
 
 <style scoped>
+
 </style>
